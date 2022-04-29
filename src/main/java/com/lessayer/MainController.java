a057c28274730589f934838755dbed9c916bf208
znkaihuang <zkhuang@protonmail.com>
2022-04-29 14:56:15 +0800

    Update view of homepage

--- src/main/java/com/lessayer/MainController.java
+++ src/main/java/com/lessayer/MainController.java
@@ -1,13 +1,15 @@
 package com.lessayer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 	
 	@GetMapping("/")
-	public String showHomePage() {
+	public String showHomePage(Model model) {
+		model.addAttribute("contactMail", "zkhuang@protonmail.com");
 		return "index";
 	}
 	
