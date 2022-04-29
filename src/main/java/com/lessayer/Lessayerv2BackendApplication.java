a1b0a764d6d87559674521039d319af707ea686e
znkaihuang <zkhuang@protonmail.com>
2022-04-29 11:14:42 +0800

    Initail commit

---
+++ src/main/java/com/lessayer/Lessayerv2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.lessayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lessayerv2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lessayerv2BackendApplication.class, args);
+	}
+
+}
