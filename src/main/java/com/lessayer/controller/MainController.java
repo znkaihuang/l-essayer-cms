ba255a1b3b0964728a6841f96a2836417ce84598
znkaihuang <zkhuang@protonmail.com>
2022-05-30 11:05:32 +0800

    Refactor directory and add article template

--- src/main/java/com/lessayer/controller/MainController.java
+++ src/main/java/com/lessayer/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.lessayer;
+package com.lessayer.controller;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
