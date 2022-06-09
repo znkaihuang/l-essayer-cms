8ab4111ecde969d41de1685946e7dffc3c7e1aaa
znkaihuang <zkhuang@protonmail.com>
2022-06-09 18:25:41 +0800

    Update filter query methods for VideoRepositry and update corresponding tests

--- src/main/java/com/lessayer/repository/VideoRepository.java
+++ src/main/java/com/lessayer/repository/VideoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.lessayer.entity.Language;
 import com.lessayer.entity.Video;
 
 public interface VideoRepository extends PagingAndSortingRepository<Video, Integer> {
@@ -14,8 +15,16 @@ public interface VideoRepository extends PagingAndSortingRepository<Video, Integ
 	@Query("SELECT v FROM Video v")
 	public List<Video> findAllVideos();
 	
-	@Query("SELECT DISTINCT v FROM Video v JOIN v.lecturer l WHERE CONCAT(v.title, ' ', v.uploader, ' ',"
+	@Query("SELECT DISTINCT v FROM Video v JOIN v.lecturer l WHERE CONCAT(v.title, ' ', v.uploader, ' ', v.date, ' ',"
 			+ " l.firstName, ' ', l.lastName, ' ', v.date, ' ', v.description, ' ', v.url) Like %?1%")
 	public Page<Video> findVideosWithKeyword(String keyword, Pageable pageable);
 	
+	@Query("SELECT v FROM Video v WHERE v.language = ?1")
+	public List<Video> findVideosWithLanguage(Language language);
+	
+	@Query("SELECT v FROM Video v WHERE v.hasSubtitle = ?1")
+	public List<Video> findVideosBySubtitle(boolean hasSubtitle);
+	
+	@Query("SELECT v FROM Video v WHERE v.videoLength >= ?1 AND v.videoLength <= ?2")
+	public List<Video> findVideosBetweenLength(Integer lowerBound, Integer upperBound);	
 }
--- src/test/java/com/lessayer/repository/VideoRepositoryTests.java
+++ src/test/java/com/lessayer/repository/VideoRepositoryTests.java
@@ -56,7 +56,7 @@ public class VideoRepositoryTests {
 	}
 	
 	@Test
-	public void createVideosTest() throws ParseException {
+	public void createVideosTest1() throws ParseException {
 		Lecturer lecturer1 = entityManager.find(Lecturer.class, 1);
 		Video video1 = new Video("Englisth speaking 1", "Lena", lecturer1, dateFormat.parse("2022-06-08"), 
 				"Englist speaking class episode 1", "enlish_speaking_1.mp4", Language.EN, true, 420);
@@ -77,10 +77,63 @@ public class VideoRepositoryTests {
 	}
 	
 	@Test
+	public void createVideosTest2() throws ParseException {
+		Lecturer lecturer1 = entityManager.find(Lecturer.class, 1);
+		Video video1 = new Video("Englisth speaking 2", "Lena", lecturer1, dateFormat.parse("2022-06-09"), 
+				"Englist speaking class episode 2", "enlish_speaking_2.mp4", Language.EN, true, 435);
+		
+		Lecturer lecturer2 = entityManager.find(Lecturer.class, 2);
+		Video video2 = new Video("French grammar 2", "Anne", lecturer2, dateFormat.parse("2022-06-11"), 
+				"Enhance French grammar", "french_grammar_2.mp4", Language.FR, false, 480);
+		Video video3 = new Video("French grammar 3", "Anne", lecturer2, dateFormat.parse("2022-06-14"), 
+				"Enhance French grammar", "french_grammar_3.mp4", Language.FR, false, 515);
+		
+		Lecturer lecturer3 = entityManager.find(Lecturer.class, 3);
+		Video video4 = new Video("Why you need to learn programming?", "Bo-nan", lecturer3, dateFormat.parse("2022-06-09"), 
+				"Start to learn program series", "why_you_need_to_learn_programming.mp4", Language.ZH_TW, true, 600);
+		Video video5 = new Video("How to learn programming?", "Bo-nan", lecturer3, dateFormat.parse("2022-06-12"), 
+				"Start to learn program series", "how_to_learn_programming.mp4", Language.ZH_TW, true, 310);
+		
+		videoRepository.save(video1);
+		videoRepository.save(video2);
+		videoRepository.save(video3);
+		videoRepository.save(video4);
+		videoRepository.save(video5);
+		
+		System.out.println("Successfully creates videos!");
+	}
+	
+	@Test
 	public void listAllVideosTest() {
 		List<Video> videoList = videoRepository.findAllVideos();
 		
 		assertThat(videoList.size()).isGreaterThan(0);
 	}
 	
+	@Test
+	public void findVideosWithLanguageTest() {
+		List<Video> videoList = videoRepository.findVideosWithLanguage(Language.EN);
+		printVideoList(videoList);
+		assertThat(videoList.size()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void findVideosBySubtitleTest() {
+		List<Video> videoList = videoRepository.findVideosBySubtitle(true);
+		printVideoList(videoList);
+		assertThat(videoList.size()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void findVideosBetweenLengthTest() {
+		List<Video> videoList = videoRepository.findVideosBetweenLength(300, 500);
+		printVideoList(videoList);
+		assertThat(videoList.size()).isGreaterThan(0);
+	}
+	
+	private void printVideoList(List<Video> videoList) {
+		for (Video video : videoList) {
+			System.out.println(video);
+		}
+	}
 }
