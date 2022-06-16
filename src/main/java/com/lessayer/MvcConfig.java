3465688618cf273808f5779417437aa95d9f3ea7
znkaihuang <zkhuang@protonmail.com>
2022-06-16 11:50:34 +0800

    Expose video directory

--- src/main/java/com/lessayer/MvcConfig.java
+++ src/main/java/com/lessayer/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		exposeDirectory("user-photos", registry);
 		exposeDirectory("article-image-files", registry);
+		exposeDirectory("videos", registry);
 	}
 	
 	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
