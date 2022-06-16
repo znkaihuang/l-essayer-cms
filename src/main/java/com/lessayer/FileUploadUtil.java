169a111ff2afcadbc6b9c7ff93b0d1b697c048f6
znkaihuang <zkhuang@protonmail.com>
2022-06-16 15:08:20 +0800

    FileUploadUtil save method supports compression option

--- src/main/java/com/lessayer/FileUploadUtil.java
+++ src/main/java/com/lessayer/FileUploadUtil.java
@@ -1,12 +1,15 @@
 package com.lessayer;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,18 +20,36 @@ public class FileUploadUtil {
 	
 	public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile)
 			throws IOException {
+		saveFile(uploadDir, fileName, multipartFile, false);
+	}
+	
+	public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile, Boolean compressed)
+			throws IOException {
 		Path uploadPath = Paths.get(uploadDir);
 		
 		if (!Files.exists(uploadPath)) {
 			Files.createDirectories(uploadPath);
 		}
 		
-		try (InputStream inpuStream = multipartFile.getInputStream()) {
-			Path filePath = uploadPath.resolve(fileName);
-			Files.copy(inpuStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+			if (compressed == false) {
+				Path filePath = uploadPath.resolve(fileName);
+				Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+			}
+			else {
+				Path filePath = uploadPath.resolve(fileName.replaceAll("[.].*", ".bz2"));
+				OutputStream out = new FileOutputStream(filePath.toFile());
+				BZip2CompressorOutputStream bzOut = new BZip2CompressorOutputStream(out);
+				final byte[] buffer = new byte[100];
+				int n = 0;
+				while (-1 != (n = inputStream.read(buffer))) {
+				    bzOut.write(buffer, 0, n);
+				}
+				bzOut.close();
+			}
 		}
 		catch (IOException ex) {
-			throw new IOException("Could not save file: " + fileName);
+			throw new IOException("Could not save file: " + fileName + ".\n" + ex);
 		}
 		
 	}
