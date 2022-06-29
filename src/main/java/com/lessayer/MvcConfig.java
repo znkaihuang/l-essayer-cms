428be2e5061894e5932981a340b7224bd7fbf566
znkaihuang <zkhuang@protonmail.com>
2022-06-29 17:56:42 +0800

    Remove Config annotation of MvcConfig class

--- src/main/java/com/lessayer/MvcConfig.java
+++ src/main/java/com/lessayer/MvcConfig.java
@@ -3,11 +3,11 @@ package com.lessayer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
